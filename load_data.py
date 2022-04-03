2ddbc3b9cc88ff4f24930a95959be1d23a60a4a8
Naomi Verkerk <n.w.verkerk@student.tudelft.nl>
2022-04-03 12:24:13 +0200

    Latests changes of the loading data

--- load_data.py
+++ load_data.py
@@ -4,8 +4,8 @@ import os
 
 def load_data():
     this_directory = os.path.dirname(os.path.abspath(__file__))
-    data = pd.read_csv(os.path.join(this_directory, 'TCGA_GBM_radiomicFeatures.csv'), index_col=0)
-    data2 = pd.read_csv(os.path.join(this_directory, 'TCGA_LGG_radiomicFeatures.csv'), index_col=0)
+    data = pd.read_csv(os.path.join(this_directory, 'TCGA_GBM_radiomicFeatures.csv'), na_values=['#DIV/0!'], index_col=0)
+    data2 = pd.read_csv(os.path.join(this_directory, 'TCGA_LGG_radiomicFeatures.csv'), na_values=['#DIV/0!'], index_col=0)
 
     data['label'] = 'GBM'
     data2['label'] = 'LGG'
